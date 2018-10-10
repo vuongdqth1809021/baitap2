b8496e61eb91d75b6925dc6b3946fedebb835a76
- <->
2018-10-10 19:38:34 +0700

    baitap 2

---
+++ cmake-build-debug/CMakeFiles/tinh_muc_luong_nhan_vien.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinh_muc_luong_nhan_vien.dir/main.c.obj"
+  "tinh_muc_luong_nhan_vien.pdb"
+  "tinh_muc_luong_nhan_vien.exe"
+  "tinh_muc_luong_nhan_vien.exe.manifest"
+  "libtinh_muc_luong_nhan_vien.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinh_muc_luong_nhan_vien.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tinh_muc_luong_nhan_vien.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tinh_muc_luong_nhan_vien.
+# This may be replaced when dependencies are built.
